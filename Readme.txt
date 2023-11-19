4d3c9ee662974284458435762dcfcc7a5a3cbb00
unknown <taliwalnishant@gmail.com>
2023-11-20 02:03:27 +0530

    minor changes

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Author: Nishant Taliwal
+
+Comments are added for easy readability.
+
+How to run the code:
+1. Pull the repo.
+2. Install Node.js.
+3. Run the command: `node questionGenerator.js`.
+
+Assumptions:
+The code is written with the assumption that total marks will be divided as a whole number in each difficulty.
--- questionGenerator.js
+++ questionGenerator.js
@@ -6,7 +6,7 @@ const questionBankPath = path.resolve(__dirname, 'questionBank.json');
 const questionBankContent = fs.readFileSync(questionBankPath, 'utf8');
 const questionBank = JSON.parse(questionBankContent).questions;
 
-
+// to randomize the questions
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -15,41 +15,43 @@ function shuffleArray(array) {
     return array;
   }
 
-
+// funtion to generate the question paper
 function generateQuestionPaper(totalMarks, difficultyDistribution) {
   const questionPaper = [];
   for (const difficulty in difficultyDistribution) {
-    const percentage = difficultyDistribution[difficulty];
-    const targetMarks = Math.floor((totalMarks * percentage) / 100);
+    const percentage = difficultyDistribution[difficulty];      
+    const targetMarks = Math.floor((totalMarks * percentage) / 100);  // cal marks as per each difficulty
 
-    const filteredQuestions = questionBank.filter(question => question.difficulty === difficulty);
+    const filteredQuestions = questionBank.filter(question => question.difficulty === difficulty);  //filtering the question acc to difficulty  
     
     
     let noOfQuestions;
     if(difficulty==="Easy") 
         noOfQuestions=targetMarks/5;
-
     else if(difficulty==="Medium")
         noOfQuestions=targetMarks/10;    
     else if(difficulty==="Hard")
         noOfQuestions=targetMarks/15;
-     console.log(`${noOfQuestions}`);
+
+
+    //  console.log(`${noOfQuestions}`);
+    
     if (filteredQuestions.length < noOfQuestions) {
       console.error(`Not enough questions with difficulty ${difficulty} in the Question Bank.`);
       return null;
     }
 
     const shuffledQuestions = shuffleArray(filteredQuestions);
-    const selectedQuestions = shuffledQuestions.slice(0, noOfQuestions);
-    // const selectedQuestions = filteredQuestions.slice(0, noOfQuestions);
-    questionPaper.push(...selectedQuestions);
+    const selectedQuestions = shuffledQuestions.slice(0, noOfQuestions);    // selection of question from randomize array
+    questionPaper.push(...selectedQuestions);       // pushing the random question in result
   }
 
   return questionPaper;
 }
 
-// Example usage
-const totalMarks = 100;
+// example test case
+
+const totalMarks = 120;
 const difficultyDistribution = { Easy: 20, Medium: 50, Hard: 30 };
 
 const generatedPaper = generateQuestionPaper(totalMarks, difficultyDistribution);
